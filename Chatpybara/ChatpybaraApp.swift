35d8228c2e58395f3913e6706af73bff608446b4
rkka <brian10kim22@gmail.com>
2025-01-28 23:46:40 +0900

    Initial Commit

---
+++ Chatpybara/ChatpybaraApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatpybaraApp.swift
+//  Chatpybara
+//
+//  Created by makisbea on 1/28/25.
+//
+
+import SwiftUI
+
+@main
+struct ChatpybaraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatpybaraTests/ChatpybaraTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChatpybaraTests.swift
+//  ChatpybaraTests
+//
+//  Created by makisbea on 1/28/25.
+//
+
+import Testing
+@testable import Chatpybara
+
+struct ChatpybaraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
